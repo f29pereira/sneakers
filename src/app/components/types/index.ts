594728164f296afcfc19136120efb681bdce1c42
Filipe Pereira <f29.pereira@gmail.com>
2026-02-09 18:53:26 +0000

    feat: add Arrow component with directional icon

--- src/app/components/types/index.ts
+++ src/app/components/types/index.ts
@@ -5,3 +5,11 @@
 export type MobileNavProps = {
   handleToggle: React.MouseEventHandler;
 };
+
+/**
+ * Props for the Arrow component
+ * @property direction - arrow direction
+ */
+export type ArrowProps = {
+  direction: "left" | "right";
+};
