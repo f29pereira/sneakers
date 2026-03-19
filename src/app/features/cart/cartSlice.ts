c0e19ac79167a99a391cef85aa96f507f4c10d17
Filipe Pereira <f29.pereira@gmail.com>
2026-03-19 17:22:28 +0000

    feat: add addToCart reducer to the redux cartSlice

--- src/app/features/cart/cartSlice.ts
+++ src/app/features/cart/cartSlice.ts
@@ -8,14 +8,28 @@ const initialState: CartState = {
 };
 
 /**
- * Short Urls slice
+ * User's shopping cart slice
  */
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     addToCart: (state, action: PayloadAction<CartItem>) => {
-      state.items.push(action.payload);
+      const itemQuantity = action.payload.quantity;
+      const itemPrice = action.payload.currentPrice;
+      const lineTotal = itemQuantity * itemPrice;
+      const isTemInCart = state.items.find(
+        (item) => item.id === action.payload.id,
+      );
+
+      if (isTemInCart) {
+        isTemInCart.quantity += itemQuantity;
+      } else {
+        state.items.push(action.payload);
+      }
+
+      state.totalQuantity += action.payload.quantity;
+      state.subTotal += lineTotal;
     },
     /*TO DO: add removeFromCart reducer*/
   },
