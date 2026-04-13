82d50daa620de32d745ad09f0dad148a953dd0f1
Filipe Pereira <f29.pereira@gmail.com>
2026-04-13 16:48:14 +0100

    test: update E2E testing for mobile navigation

--- e2e/navigation-mobile.spec.ts
+++ e2e/navigation-mobile.spec.ts
@@ -1,31 +1,77 @@
-import { test, devices, expect } from "@playwright/test";
+import { test, devices, expect, Page } from "@playwright/test";
+import { getNavLinksDesc } from "../fixtures/sneakers.fixture";
 
 test.use({ ...devices["Pixel 5"] });
 
-// TO DO: add reusable open menu function
-
-test("has mobile navigation button", async ({ page }) => {
-  await page.goto("/"); // baseURL
-
-  const hamburgerBtn = page.getByRole("button", { name: "Open Menu" });
-  await expect(hamburgerBtn).toBeVisible();
-});
-
-test("allows the user to open the mobile navigation", async ({ page }) => {
-  await page.goto("/"); // baseURL
-
+/**
+ * Open mobile navigation
+ */
+const openNavigation = async (page: Page) => {
   const hamburgerBtn = page.getByRole("button", { name: "Open Menu" });
   await hamburgerBtn.click();
+};
 
-  const menLink = page.getByRole("link", { name: "Men" });
-  const womanLink = page.getByRole("link", { name: "Woman" });
-  const aboutLink = page.getByRole("link", { name: "About" });
-  const contactLink = page.getByRole("link", { name: "Contact" });
+/**
+ * Returns movile navigation links
+ */
+const getLinks = (page: Page) => {
+  const links = getNavLinksDesc();
 
-  await expect(menLink).toBeVisible();
-  await expect(womanLink).toBeVisible();
-  await expect(aboutLink).toBeVisible();
-  await expect(contactLink).toBeVisible();
+  const collectionsLink = page.getByRole("link", {
+    name: links.collectionsLink,
+  });
+  const menLink = page.getByRole("link", { name: links.menLink });
+  const womanLink = page.getByRole("link", { name: links.womanLink });
+  const aboutLink = page.getByRole("link", { name: links.aboutLink });
+  const contactLink = page.getByRole("link", { name: links.contactLink });
+
+  return { collectionsLink, menLink, womanLink, aboutLink, contactLink };
+};
+
+/**
+ * End to End testing: mobile navigation
+ */
+test.describe("Mobile navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/"); // baseURL
+  });
+
+  test("has mobile navigation button", async ({ page }) => {
+    const hamburgerBtn = page.getByRole("button", { name: "Open Menu" });
+    await expect(hamburgerBtn).toBeVisible();
+  });
+
+  test("allows the user to open the mobile navigation", async ({ page }) => {
+    await openNavigation(page);
+
+    const closeBtn = page.getByRole("button", { name: "Close Menu" });
+
+    const { collectionsLink, menLink, womanLink, aboutLink, contactLink } =
+      getLinks(page);
+
+    await expect(closeBtn).toBeVisible();
+    await expect(collectionsLink).toBeVisible();
+    await expect(menLink).toBeVisible();
+    await expect(womanLink).toBeVisible();
+    await expect(aboutLink).toBeVisible();
+    await expect(contactLink).toBeVisible();
+  });
+
+  test("allows the user to close the mobile navigation", async ({ page }) => {
+    await openNavigation(page);
+
+    const closeBtn = page.getByRole("button", { name: "Close Menu" });
+
+    await closeBtn.click(); // Close mobile navigation
+
+    const { collectionsLink, menLink, womanLink, aboutLink, contactLink } =
+      getLinks(page);
+
+    await expect(closeBtn).not.toBeVisible();
+    await expect(collectionsLink).not.toBeVisible();
+    await expect(menLink).not.toBeVisible();
+    await expect(womanLink).not.toBeVisible();
+    await expect(aboutLink).not.toBeVisible();
+    await expect(contactLink).not.toBeVisible();
+  });
 });
-
-// TO DO: add close mobile navigation test
