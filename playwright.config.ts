fdae1ac6c47a9f982a01496bb4d6baeb5834cf4e
Filipe Pereira <f29.pereira@gmail.com>
2026-04-23 11:26:43 +0100

    ci: add Playwright to the test workflow
    - Update Playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -109,7 +109,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run build && npm run start",
+    command: "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
