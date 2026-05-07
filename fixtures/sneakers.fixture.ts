22a987aa70b50715bf10f62e472ae673422836ec
Filipe Pereira <f29.pereira@gmail.com>
2026-05-07 19:04:02 +0100

    test: add unit testing for the Notification component
    - Update Notification component with data-testid for the icons

--- fixtures/sneakers.fixture.ts
+++ fixtures/sneakers.fixture.ts
@@ -4,6 +4,7 @@ import type {
   CartItem,
   CartState,
   Product,
+  NotificationState,
 } from "@/app/components/types";
 
 /**
@@ -115,4 +116,18 @@ export const getCartState = (): CartState => {
   const item = getCartItemData();
 
   return { items: [item], totalQuantity: 3, subTotal: 125 };
+};
+
+/**
+ * Mocked data: empty notification
+ */
+export const getEmptyNotification = (): NotificationState => {
+  return { message: "", isSuccess: false };
+};
+
+/**
+ * Mocked data: success notification
+ */
+export const getSuccessNotification = (): NotificationState => {
+  return { message: "Item added to cart", isSuccess: true };
 };
--- src/app/components/ui/Notification/Notification.tsx
+++ src/app/components/ui/Notification/Notification.tsx
@@ -34,6 +34,7 @@ export default function Notification() {
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
+                  data-testid="successIcon"
                 >
                   <path d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
@@ -45,6 +46,7 @@ export default function Notification() {
                   className={styles.icon}
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
+                  data-testid="errorIcon"
                 >
                   <path d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
